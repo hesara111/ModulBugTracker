64a3cfbc4d0304c4e8c8ce7cd99e9fc2df403cc7
hesara111 <dimanyel@gmail.com>
2018-12-10 19:54:30 +0200

    StartProg

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursModul.Net_2nd_mounth_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursModul.Net_2nd_mounth_
+{
+    class Task
+    {
+        public int number { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
