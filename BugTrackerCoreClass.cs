ee387073462659c69f1dd89b6e3bf2e2b9479313
dmitrishein <drghost9899@gmail.com>
2018-12-12 02:56:56 +0200

    Update BugTrackerCoreClass.cs

--- BugTrackerCoreClass.cs
+++ BugTrackerCoreClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Alpha
 {
-    class BugTrackerCoreClass<T>
+    class BugTrackerCoreClass<T> where T:Task
     {
         public List<T> Bugs = new List<T>();//список багов
         public List<T> Features = new List<T>();//список фичь
@@ -30,26 +30,52 @@ namespace Alpha
         }
         public void AddFeature(T feature)
         {
-
+            feature.status = "ToDo";
             Features.Add(feature);
         }
         public void AddBug(T bug)
-        {
+        { bug.status = "ToDo";
             Bugs.Add(bug);
         }
         public void AddTechDeb(T td)
         {
+            td.status = "ToDo";
             TechDeb.Add(td);
         }
-        public void Iteration(int iterationAmount)
+        public void Iteration(int taskAmount,int execution )
         {
-            for(int i=0;i<iterationAmount;i++)
+            for(int i=0;i<execution;i++)
             {
-                foreach(var item in Bugs)
-                {
+                Random rd = new Random();
 
-                }
-                foreach(var item in Features)
+                foreach (var item in Features.Take(taskAmount))
+                {
+                    item.status = "In Progress";
+                    if(rd.Next(1,(item.complexity*item.priority))==(item.complexity*item.priority))
+                    {
+                          if(Bugs.Contains(item)==false)
+                        {
+                            item.status = "ToDo";
+                            Bugs.Add(item);
+                            item.status = "In Progress";
+                            
+                          }//если фичи нет в багах
+
+                    }//высчитывается вероятность появления баги=а
+                    else
+                    {
+                        item.TimeOfExecution--;
+                    }    
+                    if(item.TimeOfExecution==0&& Bugs.Contains(item) == false)
+                    {
+                        item.status = "Done";
+                        Done.Add(item);
+                        Features.Remove(item);
+                        
+                    }//в случае выполнения операции и выполнения бага
+                
+                }//обработка фичь
+                foreach (var item in Bugs)
                 {
 
                 }
